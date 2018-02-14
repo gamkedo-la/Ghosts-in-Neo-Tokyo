9b93e6f617c1a630db2983362cb4dbc914d53a06
ClayTaeto <silentstrum@gmail.com>
2018-02-13 19:17:19 -0800

    REfactor first steps

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -29,45 +29,14 @@ function loadImageForWorldCode(worldCode, fileName) {
 	worldPics[worldCode].src = "img/"+ fileName;
 }
 
-function loadImages() {
-	var imageList = [
-		{set: "Player", name: "stand", fileName: "MainChar/Char_Animation 1.png"},
-		{set: "Player", name: "walkSouth", fileName: "MainChar/Char_Animation 2.png"},
-		{set: "Player", name: "walkWest", fileName: "MainChar/Char_Animation 3.png"},
-		{set: "Player", name: "walkEast", fileName: "MainChar/Char_Animation 4.png"},
-		{set: "Player", name: "attackDown", fileName: "MainChar/Char_Animation 5.png"},
-		{set: "Concept", name: "background1", fileName: "Concept.png"},
-		// {set: "Player", name: "walkSouth", fileName: "MainChar/playerchar_Walk South.png"},
-		// {set: "Player", name: "walkEast", fileName: "MainChar/playerchar_Walk East.png"},
-		// {set: "Player", name: "walkNorth", fileName: "MainChar/playerchar_Walk North.png"},
-		// {set: "Player", name: "walkWest", fileName: "MainChar/playerchar_Walk West.png"},
-		// {set: "Player", name: "anchorAttack", fileName: "MainChar/Anchor Attack.png"},
+function loadImages() {	
+	picsToLoad = imgPayload.length;
 
-		// {set: "HeroBoss", name: "Stand", fileName: "HeroBoss/Boss_stand.png"},
-		
-
-		// {set: "Ending", name: "tempEndScreen", fileName: "temp-endscreen.png"},
-		// {set: "OPENING", name: "tempOpening", fileName: "playtest splash.png"},
-		// {set: "OPENING", name: "controls", fileName: "Controls Screen.png"},
-		// {set: "OPENING", name: "cursor", fileName: "cursor.png"},
-		
-		// {set: "Credits", name: "page1", fileName: "Credits/Page1.png"},
-		// {set: "Credits", name: "page2", fileName: "Credits/Page2.png"},
-		// {set: "Credits", name: "page3", fileName: "Credits/Page3.png"},
-		// {set: "Credits", name: "page4", fileName: "Credits/Page4.png"},
-
-		{worldType: TILE_EMPTY, theFile: "Tiles/world_nothingness.png"},
-
-		
-	];
-
-	picsToLoad = imageList.length;
-
-	for(var i=0;i<imageList.length;i++) {
-		if(imageList[i].set != undefined, imageList[i].set != undefined) {
-			beginLoadingImage(imageList[i]);
+	for(var i=0;i<imgPayload.length;i++) {
+		if(imgPayload[i].set != undefined, imgPayload[i].set != undefined) {
+			beginLoadingImage(imgPayload[i]);
 		} else {
-			loadImageForWorldCode(imageList[i].worldType, imageList[i].theFile);
+			loadImageForWorldCode(imgPayload[i].worldType, imgPayload[i].theFile);
 		}
 	}
 }
