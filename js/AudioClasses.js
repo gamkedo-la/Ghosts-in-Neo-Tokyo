2cd3c5cde742eb13e94357c048101976a629d305
M D Fewkes <mdfewkes@gmail.com>
2018-02-21 10:32:42 -0700

    Added music classes and manager

--- js/AudioClasses.js
+++ js/AudioClasses.js
@@ -1,5 +1,7 @@
-var sfxVolume = 1;
 var isMuted = false;
+
+//SFX Classes
+var sfxVolume = 1;
 
 function setSFXVolume(amount) {
 	sfxVolume = amount;
@@ -209,5 +211,512 @@ function sfxClipOverlap(filenameWithPath, voices) {
 
 	this.getPaused = function() {
 		return soundFile[currentClip].paused;
+	}
+}
+
+//Music Classes
+var musicVolume = 1;
+
+function setMusicVolume(amount) {
+	musicVolume = amount;
+}
+
+function getMusicVolume() {
+	return musicVolume;
+}
+
+function musicTrackLooping(filenameWithPath) {
+	var musicFile = new Audio(filenameWithPath+audioFormat);
+	var duration = musicFile.duration;
+	var trackName = filenameWithPath;
+	var trackVolume = 1;
+	var mixVolume = 1;
+
+	musicFile.pause();
+	musicFile.loop = true;
+
+	this.play = function() {
+		musicFile.currentTime = 0;
+		this.updateVolume();
+		musicFile.play();
+	}
+
+	this.stop = function() {
+		musicFile.pause();
+		musicFile.currentTime = 0;
+	}
+
+	this.resume = function() {
+		musicFile.play();
+	}
+
+	this.pause = function() {
+		musicFile.pause();
+	}
+
+	this.playFrom = function(time) {
+		musicFile.currentTime = time;
+		musicFile.play();
+	}
+
+	this.startOrStop = function() {
+		if(musicFile.paused) {
+			this.resume();
+		} else {
+			this.pause();
+		}
+	}
+
+	this.updateVolume = function() {
+		musicFile.volume = Math.pow(mixVolume * musicVolume  * trackVolume * !isMuted, 2);
+	}
+
+	this.setVolume = function(newVolume) {
+		if(newVolume > 1) {newVolume = 1;}
+		if(newVolume < 0) {newVolume = 0;}
+		musicFile.volume = Math.pow(mixVolume * newVolume * musicVolume * !isMuted, 2);
+		trackVolume = newVolume;
+	}
+
+	this.getVolume = function() {
+		return trackVolume * !isMuted;
+	}
+
+	this.setMixVolume = function(volume) {
+		mixVolume = volume;
+	}
+
+	this.getMixVolume = function() {
+		return mixVolume;
+	}
+
+	this.setTime = function(time) {
+		musicFile.currentTime = time;
+	}
+
+	this.getTime = function() {
+		return musicFile.currentTime;
+	}
+
+	this.setPlaybackRate = function(rate) {
+		musicFile.playbackRate = rate;
+	}
+
+	this.getPlaybackRate = function() {
+		return musicFile.playbackRate;
+	}
+	
+	this.setTrackName = function(name) {
+		trackName = name;
+	}
+
+	this.getTrackName = function() {
+		return trackName;
+	}
+	
+	this.getDuration = function() {
+		return duration;
+	}
+
+	this.getPaused = function() {
+		return musicFile.paused;
+	}
+}
+
+function musicTrackLoopingWTail(filenameWithPath, playLength) {
+	var musicFile = new Array(new Audio(filenameWithPath+audioFormat), new Audio(filenameWithPath+audioFormat));
+	var currentTrack = 0;
+	var duration = playLength;
+	var trackName = filenameWithPath;
+	var trackVolume = 1;
+	var mixVolume = 1;
+
+	musicFile[0].pause();
+	musicFile[1].pause();
+
+	this.play = function() {
+		musicFile[currentTrack].currentTime = 0;
+		this.updateVolume();
+		musicFile[currentTrack].play();
+		musicManager.addTimerEvent(this, "loop");
+	}
+
+	this.stop = function() {
+		musicFile[0].pause();
+		musicFile[0].currentTime = 0;
+		musicFile[1].pause();
+		musicFile[1].currentTime = 0;
+	}
+
+	this.resume = function() {
+		musicFile[currentTrack].play();
+		musicManager.addTimerEvent(this, "loop");
+	}
+
+	this.pause = function() {
+		musicFile[0].pause();
+		musicFile[1].pause();
+	}
+
+	this.playFrom = function(time) {
+		musicFile[currentTrack].currentTime = time;
+		musicFile[currentTrack].play();
+		musicManager.addTimerEvent(this, "loop");
+	}
+
+	this.startOrStop = function() {
+		if(musicFile[currentTrack].paused) {
+			this.resume();
+		} else {
+			this.pause();
+		}
+	}
+
+	this.triggerTimerEnded = function(callSign) {
+		currentTrack++;
+		if (currentTrack > 1) {currentTrack = 0;}
+		this.play();
+	}
+
+	this.updateVolume = function() {
+		musicFile[0].volume = Math.pow(mixVolume * musicVolume  * trackVolume * !isMuted, 2);
+		musicFile[1].volume = Math.pow(mixVolume * musicVolume  * trackVolume * !isMuted, 2);
+	}
+
+	this.setVolume = function(newVolume) {
+		if(newVolume > 1) {newVolume = 1;}
+		if(newVolume < 0) {newVolume = 0;}
+		musicFile[currentTrack].volume = Math.pow(mixVolume * newVolume * musicVolume * !isMuted, 2);
+		trackVolume = newVolume;
+	}
+
+	this.getVolume = function() {
+		return trackVolume * !isMuted;
+	}
+
+	this.setMixVolume = function(volume) {
+		mixVolume = volume;
+	}
+
+	this.getMixVolume = function() {
+		return mixVolume;
+	}
+
+	this.setTime = function(time) {
+		musicFile[currentTrack].currentTime = time;
+	}
+
+	this.getTime = function() {
+		return musicFile[currentTrack].currentTime;
+	}
+
+	this.setPlaybackRate = function(rate) {
+		musicFile[0].playbackRate = rate;
+		musicFile[1].playbackRate = rate;
+	}
+
+	this.getPlaybackRate = function() {
+		return musicFile[currentTrack].playbackRate;
+	}
+	
+	this.setTrackName = function(name) {
+		trackName = name;
+	}
+
+	this.getTrackName = function() {
+		return trackName;
+	}
+	
+	this.getDuration = function() {
+		return duration;
+	}
+
+	this.getPaused = function() {
+		return musicFile[currentTrack].paused;
+	}
+}
+
+function musicTrackStinger(filenameWithPath) {
+	var musicFile = new Audio(filenameWithPath+audioFormat);
+	var duration = musicFile.duration;
+	var trackName = filenameWithPath;
+	var trackVolume = 1;
+	var mixVolume = 1;
+
+	musicFile.pause();
+	musicFile.loop = false;
+
+	this.play = function() {
+		musicFile.currentTime = 0;
+		this.updateVolume();
+		musicFile.play();
+	}
+
+	this.stop = function() {
+		musicFile.pause();
+		musicFile.currentTime = 0;
+	}
+
+	this.resume = function() {
+		musicFile.play();
+	}
+
+	this.pause = function() {
+		musicFile.pause();
+	}
+
+	this.playFrom = function(time) {
+		musicFile.currentTime = time;
+		musicFile.play();
+	}
+
+	this.startOrStop = function() {
+		if(musicFile.paused) {
+			this.resume();
+		} else {
+			this.pause();
+		}
+	}
+
+	this.updateVolume = function() {
+		musicFile.volume = Math.pow(mixVolume * musicVolume  * trackVolume * !isMuted, 2);
+	}
+
+	this.setVolume = function(newVolume) {
+		if(newVolume > 1) {newVolume = 1;}
+		if(newVolume < 0) {newVolume = 0;}
+		musicFile.volume = Math.pow(mixVolume * newVolume * musicVolume * !isMuted, 2);
+		trackVolume = newVolume;
+	}
+
+	this.getVolume = function() {
+		return trackVolume * !isMuted;
+	}
+
+	this.setMixVolume = function(volume) {
+		mixVolume = volume;
+	}
+
+	this.getMixVolume = function() {
+		return mixVolume;
+	}
+
+	this.setTime = function(time) {
+		musicFile.currentTime = time;
+	}
+
+	this.getTime = function() {
+		return musicFile.currentTime;
+	}
+
+	this.setPlaybackRate = function(rate) {
+		musicFile.playbackRate = rate;
+	}
+
+	this.getPlaybackRate = function() {
+		return musicFile.playbackRate;
+	}
+	
+	this.setTrackName = function(name) {
+		trackName = name;
+	}
+
+	this.getTrackName = function() {
+		return trackName;
+	}
+	
+	this.getDuration = function() {
+		return duration;
+	}
+
+	this.getPaused = function() {
+		return musicFile.paused;
+	}
+}
+
+function musicContainer(track) {
+	var musicTrack = track;
+	var trackVolume = 1;
+
+	this.play = function() {
+		musicTrack.play();
+	}
+
+	this.stop = function() {
+		musicTrack.stop();
+	}
+
+	this.resume = function() {
+		musicTrack.resume();
+	}
+
+	this.pause = function() {
+		musicTrack.pause();
+	}
+
+	this.playFrom = function(time) {
+		musicTrack.playFrom(time);
+	}
+
+	this.startOrStop = function() {
+		musicTrack.startOrStop();
+	}
+
+	this.loadTrack = function(newTrack) {
+		var timeNow = musicTrack.getTime();
+		if(!musicTrack.getPaused()) {
+			musicTrack.pause();
+			musicTrack.setTime(0);
+			musicTrack = newTrack;
+			musicTrack.setVolume(trackVolume);
+			musicTrack.playFrom(timeNow);
+		} else {
+			musicTrack = newTrack;
+			musicTrack.setVolume(trackVolume);
+			musicTrack.setTime(timeNow);
+		}
+	}
+
+	this.updateVolume = function() {
+		musicTrack.updateVolume();
+	}
+
+	this.setVolume = function(newVolume) {
+		trackVolume = newVolume;
+		musicTrack.setVolume(newVolume);
+	}
+
+	this.getVolume = function() {
+		return musicTrack.getVolume();
+	}
+
+	this.setTime = function(time) {
+		musicTrack.setTime(time);
+	}
+
+	this.getTime = function() {
+		return musicTrack.getTime();
+	}
+
+	this.setPlaybackRate = function(rate) {
+		musicTrack.setPlaybackRate(rate);
+	}
+
+	this.getPlaybackRate = function() {
+		return musicTrack.getPlaybackRate();
+	}
+	
+	this.setTrackName = function(name) {
+		musicTrack.setTrackName(name);
+	}
+
+	this.getTrackName = function() {
+		return musicTrack.getTrackName();
+	}
+	
+	this.getDuration = function() {
+		return musicTrack.getDuration();
+	}
+
+	this.getPaused = function() {
+		return musicTrack.getPaused();
+	}
+}
+
+function musicContainerCrossfade(track1, track2) {
+	var musicTrack = new Array(track1, track2);
+	var currentTrack = 0;
+	var trackVolume = 1;
+
+	this.play = function() {
+		musicTrack[currentTrack].play();
+	}
+
+	this.stop = function() {
+		musicTrack[0].stop();
+		musicTrack[1].stop();
+	}
+
+	this.resume = function() {
+		musicTrack[currentTrack].resume();
+	}
+
+	this.pause = function() {
+		musicTrack[0].pause();
+		musicTrack[1].pause();
+	}
+
+	this.playFrom = function(time) {
+		musicTrack[currentTrack].playFrom(time);
+	}
+
+	this.startOrStop = function() {
+		musicTrack[0].startOrStop();
+		musicTrack[1].startOrStop();
+	}
+
+	this.loadTrack = function(newTrack, fadeTime = 1) {
+		var timeNow = musicTrack[currentTrack].getTime();
+		var altTrack = Math.abs(currentTrack - 1);
+		if(musicTrack[currentTrack].getPaused() == false) {
+			musicTrack[altTrack] = newTrack;
+			musicTrack[altTrack].setVolume(0);
+			musicTrack[altTrack].playFrom(timeNow);
+			musicManager.removeStopEvent(musicTrack[currentTrack]);
+			musicManager.addFadeEvent(musicTrack[currentTrack], fadeTime, 0);
+			musicManager.addFadeEvent(musicTrack[altTrack], fadeTime, trackVolume);
+			musicManager.addStopEvent(musicTrack[currentTrack], fadeTime);
+			currentTrack = altTrack;
+		} else {
+			musicTrack[currentTrack] = newTrack;
+			musicTrack[currentTrack].stop();
+			musicTrack[currentTrack].setTime(timeNow);
+		}
+	}
+
+	this.updateVolume = function() {
+		musicTrack[0].updateVolume();
+		musicTrack[1].updateVolume();
+	}
+
+	this.setVolume = function(newVolume) {
+		trackVolume = newVolume;
+		musicTrack[currentTrack].setVolume(newVolume);
+	}
+
+	this.getVolume = function() {
+		return musicTrack[currentTrack].getVolume();
+	}
+
+	this.setTime = function(time) {
+		musicTrack[currentTrack].setTime(time);
+	}
+
+	this.getTime = function() {
+		return musicTrack[currentTrack].getTime();
+	}
+
+	this.setPlaybackRate = function(rate) {
+		musicTrack[currentTrack].setPlaybackRate(rate);
+	}
+
+	this.getPlaybackRate = function() {
+		return musicTrack[currentTrack].getPlaybackRate();
+	}
+	
+	this.setTrackName = function(name) {
+		musicTrack[currentTrack].setTrackName(name);
+	}
+
+	this.getTrackName = function() {
+		return musicTrack[currentTrack].getTrackName();
+	}
+	
+	this.getDuration = function() {
+		return musicTrack[currentTrack].getDuration();
+	}
+
+	this.getPaused = function() {
+		return musicTrack[currentTrack].getPaused();
 	}
 }
--- js/AudioManager.js
+++ js/AudioManager.js
@@ -1,9 +1,12 @@
 setFormat();
 isMuted = false;
 
+//declare sound assets
 var player_jump_SFX = new sfxClipSingle("./audio/player_jump");
 var player_hit_SFX = new sfxClipSingle("./audio/player_hit");
 
+
+//general audio finctions
 function setFormat() {
 	var audio = new Audio();
 	if (audio.canPlayType("audio/ogg")) {
@@ -23,4 +26,158 @@ function setMute(TorF) {
 
 function getMute(TorF) {
 	return isMuted;
+}
+
+//Music Manager
+const REMOVE = 0; // Arrayformat [REMOVE]
+const FADE = 1; // Arrayformat [FADE, track, startTime, endTime, startVolume, endVolume]
+const TIMER = 2; // Arrayformat [TIMER, track, endTime, callSign]
+const STOP = 3; // Arrayformat [STOP, track, endTime]
+
+var musicManager = new musicEventManager();
+
+function musicEventManager() {
+	var eventList = [];
+	var now = Date.now();
+
+	this.returnEventList = function() {
+		return eventList;
+	}
+
+	this.updateEvents = function() {
+		now = Date.now();
+		runList();
+		cleanupList();
+	}
+
+	this.addFadeEvent = function(track, duration, endVol) {
+		var check = checkListFor(FADE, track);
+		var endTime = duration * 1000 + now;
+		var startVolume = track.getVolume();
+		console.log("Adding Fade Event for " + track.getTrackName());
+
+		if (check == "none") {
+			eventList.push([FADE, track, now, endTime, startVolume, endVol]);
+		} else {
+			eventList[check] = [FADE, track, now, endTime, startVolume, endVol];
+		}
+	}
+
+	this.addTimerEvent = function(track, callSign = "none") {
+		var thisTrack = track;
+		var check = checkListFor(TIMER, thisTrack, callSign);
+		var endTime = (thisTrack.getDuration() - thisTrack.getTime()) * 1000 + now;
+
+		if (check == "none") {
+			console.log("Adding Timer Event for " + track.getTrackName());
+			eventList.push([TIMER, track, endTime, callSign]);
+		} else {
+			eventList[check] = [TIMER, track, endTime, callSign];
+		}
+	}
+
+	this.addStopEvent = function(track) {
+		var thisTrack = track;
+		var check = checkListFor(STOP, thisTrack);
+		var endTime = (thisTrack.getDuration() - thisTrack.getTime()) * 1000 + now;
+
+		if (check == "none") {
+			console.log("Adding Stop Event for " + track.getTrackName());
+			eventList.push([STOP, track, endTime]);
+		} else {
+			eventList[check] = [STOP, track, endTime];
+		}
+	}
+
+	this.removeStopEvent = function(track) {
+		var thisTrack = track;
+		var check = checkListFor(STOP, thisTrack);
+
+		if (check == "none") {
+			return;
+		} else {
+			console.log("Removing Stop Event for " + track.getTrackName());
+			eventList[check] = [REMOVE];
+		}
+	}
+
+	function runList(){
+		for (var i = 0; i < eventList.length; i++) {
+			if (eventList[i][0] == FADE) {
+				thisTrack = eventList[i][1];
+				if (thisTrack.getPaused() == false) {
+						thisTrack.setVolume(interpolateFade(eventList[i][2], eventList[i][3], eventList[i][4], eventList[i][5], now));
+					if (eventList[i][3] < now) {
+						console.log("Ending Fade Event for " + thisTrack.getTrackName());
+						eventList[i] = [REMOVE];
+					}
+				}
+			}
+			if (eventList[i][0] == TIMER) {
+				thisTrack = eventList[i][1];
+				if (thisTrack.getPaused() == false) {
+					if (eventList[i][2] <= now) {
+						console.log("Ending Timer Event for " + thisTrack.getTrackName());
+						eventList[i] = [REMOVE];
+						thisTrack.triggerTimerEnded(eventList[i][3]);
+					}
+				} else {
+					eventList[i] = [REMOVE];
+				}
+			}
+			if (eventList[i][0] == STOP) {
+				thisTrack = eventList[i][1];
+				if (thisTrack.getPaused() == false) {
+					if (eventList[i][2] <= now) {
+						console.log("Executing Stop Event for " + thisTrack.getTrackName());
+						eventList[i] = [REMOVE];
+						thisTrack.stop();
+					}
+				}
+			}
+		}
+
+	}
+
+	function cleanupList() {
+		eventList.sort(function(a, b){return b-a});
+		while (eventList[eventList.length - 1] == REMOVE) {
+			eventList.pop();
+		}
+	}
+
+	function checkListFor(eventType, track, callSign = ""){
+		var foundItem = false;
+		for (var i = 0; i < eventList.length; i++) {
+			if (eventList[i][0] == eventType) {
+				if (eventList[i][1] == track) {
+					if(eventType == TIMER && eventList[i][3] == callSign) {
+						foundItem = true;
+						return i;
+					} else if (eventType != TIMER) {
+						foundItem = true;
+						return i;
+					}
+				}
+			}
+		}
+		if (!foundItem) {
+			return "none";
+		}
+	}
+}
+
+function interpolateFade(startTime, endTime, startVolume, endVolume, now) {
+	var finish = endTime - startTime;
+	var currentTime = endTime - now;
+	var offset = Math.min(startVolume, endVolume);
+	var scale = Math.max(startVolume, endVolume) - offset;
+	var output = startVolume;
+
+	if (startVolume >= endVolume) {
+		output = (startVolume - offset - (1 -currentTime/finish)) * scale + offset;
+	} else {
+		output = Math.abs(startVolume - offset - (1 -currentTime/finish)) * scale + offset;
+	}
+	return output;
 }
