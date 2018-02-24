d3eb4d2e5a386ea7a8ab60f8b825e0dcb81165e0
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-02-24 23:36:12 +0100

    Removed unneeded (anymore) wave file.

--- js/AudioManager.js
+++ js/AudioManager.js
@@ -4,7 +4,7 @@ isMuted = false;
 //declare sound assets
 var player_jump_SFX = new sfxClipSingle("./audio/player_jump");
 var player_hit_SFX = new sfxClipSingle("./audio/player_hit");
-var player_hit_ground_SFX = new sfxClipSingle("./audio/player_hit_ground", ".wav");
+var player_hit_ground_SFX = new sfxClipSingle("./audio/player_hit_ground");
 
 player_jump_SFX.setVolume(0.5);
 player_hit_SFX.setVolume(0.5);
