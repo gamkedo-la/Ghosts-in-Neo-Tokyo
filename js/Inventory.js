982a875dbeccfc770069f89f54e6ec53ae672523
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-02-24 19:07:15 -0500

    Draw a placeholder inventory menu

--- js/Inventory.js
+++ js/Inventory.js
@@ -1,12 +1,31 @@
-console.log('connected');
-
 function drawInventory() {
-
-  console.log(canvasContext.currentTransform);
+  var menuX = 40;
+  var menuY = 20;
+  var menuWidth = 240;
+  var menuHeight = 140;
 
   canvasContext.save();
-  canvasContext.fillStyle = 'white';
-  canvasContext.rect(100, 50, 150, 100);
-  canvasContext.fill();
+  canvasContext.fillStyle = 'black';
+  canvasContext.strokeStyle = 'white';
+  canvasContext.strokeRect(menuX, menuY, menuWidth, menuHeight);
+  canvasContext.fillRect(menuX, menuY, menuWidth, menuHeight);
+
+  var numItems = 12;
+  var numCols = 4;
+  var itemCellWidth = 30;
+  var itemCellHeight = 30
+  var itemCellMarginX = 25;
+  var itemCellMarginY = 15;
+  var itemCellBetweenX = 25;
+  var itemCellBetweenY = 10;
+  for (var i = 0; i < numItems; i++) {
+    var itemCellX = menuX + itemCellMarginX + ((itemCellWidth + itemCellBetweenX) * (i % numCols));
+    var itemCellY = menuY + itemCellMarginY + ((itemCellHeight + itemCellBetweenY) * Math.floor(i / numCols));
+    canvasContext.save();
+    canvasContext.fillStyle = 'yellow';
+    canvasContext.fillRect(itemCellX, itemCellY, itemCellWidth, itemCellHeight);
+    canvasContext.restore();
+  }
+
   canvasContext.restore();
 }
